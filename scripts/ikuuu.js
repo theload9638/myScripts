4647284d1fc88a9923564d4e094c2d9b87accd28
kl <2967861744@qq.com>
2025-10-22 19:59:53 +0800

    34

--- scripts/ikuuu.js
+++ scripts/ikuuu.js
@@ -13,6 +13,7 @@ if (/^https?:\/\/ikuuu\.de\/user\/profile/.test(url)) {
         } else {
             const arr = obj.split('&');
             console.log(`当前cookie数量 =${arr['length']}`);
+            console.log(obj);
             if (!arr.includes(emailKey)) {
                 obj = obj + '&' + emailKey;
                 $prefs.setValueForKey(obj, key);
