a9d658ba7c6308857b714aa188bfb20b7949795f
kl <2967861744@qq.com>
2025-10-22 19:16:02 +0800

    65111211112

--- scripts/ikuuu.js
+++ scripts/ikuuu.js
@@ -8,7 +8,18 @@ if(/^https?:\/\/ikuuu\.de\/user\/profile/.test(url)){
     if($prefs.setValueForKey(e, emailKey)){
         $notify('获取Cookie成功', '',`key=${emailKey}`);
         const obj = $prefs.valueForKey(key);
-        console.log(obj);
+        if(obj===undefined){
+            obj = [];
+            obj.push(emailKey);
+            $prefs.setValueForKey(JSON.stringify(obj),key);
+        }else{
+            const arr = JSON.parse(obj);
+            console.log('当前cookie数量',arr?.length);
+            if(Array.isArray(arr) && !arr.includes(emailKey)){
+                arr.push(emailKey);
+                $prefs.setValueForKey(JSON.stringify(arr), key);
+            }
+        }
 
     }else{
         $notify('获取Cookie失败', 'error','请检查脚本');
