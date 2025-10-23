43e56de01a9036d89628372af036dfb08c478bf1
kl <2967861744@qq.com>
2025-10-24 01:06:31 +0800

    121

--- scripts/qjqx.js
+++ scripts/qjqx.js
@@ -1,15 +1,20 @@
 //wxapplet ddcx - qjqx
 //cms\.htw\.delivery\.batch
 
+console.log(($request.url).split('?')[1].substr(4));
+
 let body = JSON.parse($response.body);
 //电单车底部卡片
-body.data['qj-wxapplet-home-activity-center-hm-banner'] = {};
+if(body.data['qj-wxapplet-home-activity-center-hm-banner']){
+    body.data['qj-wxapplet-home-activity-center-hm-banner'].isAd=0;
+}
+// body.data['qj-wxapplet-home-activity-center-hm-banner'] = {};
 //单车底部的卡片
-body.data['qj-wxapplet-home-activity-center-htw-banner'] = {};
+// body.data['qj-wxapplet-home-activity-center-htw-banner'] = {};
 //单车首页弹窗
-body.data['bike-homepage-popup'] = {};
+// body.data['bike-homepage-popup'] = {};
 //广告
-body.data['h5-home-weixin-ads'] = {};
+// body.data['h5-home-weixin-ads'] = {};
 //电单车的领券微信 和一些可能的广告弹窗
 // body.data['qj-wxapplet-home-hm-daily-banner'] = {};
 // body.data['qj-wxapplet-home-htw-large-banner'].isAd = 0;
