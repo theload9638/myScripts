849b3ffa68a8358ab09943d3b45f446cb660ce7e
kl <2967861744@qq.com>
2025-10-24 00:10:31 +0800

    111

--- scripts/qjqx.js
+++ scripts/qjqx.js
@@ -5,7 +5,7 @@ const url = $request.url;
 let apiUrl = url.split('?')[1].substr(4);
 if(/cms\.htw\.delivery\.batch/.test(apiUrl)){
     let body = JSON.parse($response.body);
-    body.data={};
+    body.data['qj-wxapplet-home-activity-center-hm-banner']={};
     $done({body:JSON.stringify(body)});
 }else{
     $done({});
