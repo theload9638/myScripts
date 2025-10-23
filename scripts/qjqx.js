550b36e378735ddd0b72c5a5263a037aed62269a
kl <2967861744@qq.com>
2025-10-24 00:31:27 +0800

    x211

--- scripts/qjqx.js
+++ scripts/qjqx.js
@@ -1,16 +1,21 @@
 //wxapplet ddcx - qjqx
 //cms\.htw\.delivery\.batch
 
-const url = $request.url;
-let apiUrl = url.split('?')[1].substr(4);
-if(/cms\.htw\.delivery\.batch/.test(apiUrl)){
-    let body = JSON.parse($response.body);
-    body.data['qj-wxapplet-home-activity-center-hm-banner']={};
-    $done({body:JSON.stringify(body)});
-}else{
-    $done({});
-}
-
+let body = JSON.parse($response.body);
+//电单车底部卡片
+body.data['qj-wxapplet-home-activity-center-hm-banner'] = {};
+//单车底部的卡片
+body.data['qj-wxapplet-home-activity-center-htw-banner'] = {};
+//单车首页弹窗
+body.data['bike-homepage-popup'] = {};
+//广告
+body.data['h5-home-weixin-ads'] = {};
+//电单车的领券微信 和一些可能的广告弹窗
+body.data['qj-wxapplet-home-hm-daily-banner'] = {};
+body.data['qj-wxapplet-home-htw-large-banner']?.isAd = 0;
+body.data['qj-wxapplet-home-hm-large-banner']?.isAd = 0;
+body.data['qj-wxapplet-home-htw-daily-banner']?.isAd = 0;
+$done({ body: JSON.stringify(body) });
 
 
 
