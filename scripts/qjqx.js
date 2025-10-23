90286c35e67fb59d847abcb1cf80ab98b26fd665
kl <2967861744@qq.com>
2025-10-24 00:43:49 +0800

    asd2

--- scripts/qjqx.js
+++ scripts/qjqx.js
@@ -11,10 +11,10 @@ body.data['bike-homepage-popup'] = {};
 //广告
 body.data['h5-home-weixin-ads'] = {};
 //电单车的领券微信 和一些可能的广告弹窗
-// body.data['qj-wxapplet-home-hm-daily-banner'] = {};
-// body.data['qj-wxapplet-home-htw-large-banner']?.isAd = 0;
-// body.data['qj-wxapplet-home-hm-large-banner']?.isAd = 0;
-// body.data['qj-wxapplet-home-htw-daily-banner']?.isAd = 0;
+body.data['qj-wxapplet-home-hm-daily-banner'] = {};
+body.data['qj-wxapplet-home-htw-large-banner'] && (body.data['qj-wxapplet-home-htw-large-banner'].isAd = 0);
+body.data['qj-wxapplet-home-hm-large-banner'] && (body.data['qj-wxapplet-home-hm-large-banner'].isAd=0);
+body.data['qj-wxapplet-home-htw-daily-banner'] && (body.data['qj-wxapplet-home-htw-daily-banner'].isAd=0);
 $done({ body: JSON.stringify(body) });
 
 
