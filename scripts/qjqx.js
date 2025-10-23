94ce83e4fbc3eec86b25582c903883e7a2b528e1
kl <2967861744@qq.com>
2025-10-24 00:05:49 +0800

    xxxxa

--- scripts/qjqx.js
+++ scripts/qjqx.js
@@ -2,9 +2,15 @@
 //cms\.htw\.delivery\.batch
 
 const url = $request.url;
-console.log(url);
+let apiUrl = url.split('?')[1].substr(4);
+if(/cms\.htw\.delivery\.batch/.test(apiUrl)){
+    let body = JSON.parse($response.body);
+    body.data={};
+    $done({body:JSON.stringify(body)});
+}else{
+    $done({});
+}
 
-$done({});
 
 
 
