89d08de32a01c3c5d601688482317571a4cadf00
kl <2967861744@qq.com>
2025-10-24 15:46:05 +0800

    111

--- scripts/ikuuu.js
+++ scripts/ikuuu.js
@@ -1,11 +1,11 @@
-const url = $request.url;
 const headers = $request.headers;
 const key = 'ikuuu';
 
-console.log(url);
-if (/^https?:\/\/ikuuu\.de\/user\/profile/.test(url)) {
     const e = headers['Cookie'];
     console.log(e);
+    const email = e.replace(/.*(email=(.*?);).*/,"$2");
+    console.log(email);
+    console.log(Date.now());
     const emailKey = (e.split(';')[0]).split('=')[1].replace('%40', '@');
     if ($prefs.setValueForKey(e, emailKey)) {
         $notify('获取Cookie成功', '', `key=${emailKey}`);
@@ -22,7 +22,7 @@ if (/^https?:\/\/ikuuu\.de\/user\/profile/.test(url)) {
     } else {
         $notify('获取Cookie失败', 'error', '请检查脚本');
     }
-}
+
 $done({});
 
 
