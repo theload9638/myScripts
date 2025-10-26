c28b5c912b9d043dd151e8cfed19150aeda547a3
kl <2967861744@qq.com>
2025-10-26 18:39:08 +0800

    ik1

--- README.md
+++ README.md
@@ -38,10 +38,53 @@
    - statusCode //状态码,例如 'HTTP/1.1 200 OK'
    - headers
    - body
-6.'$done({path: , headers : ,status: ,body: });' 
+6.`$done({
+     path: String,
+     headers : Object,
+     status: String,
+     body: String,
+     bodyBytes: ArrayBuffer
+     });`
    - //释放脚本,终止本次执行
-   - //$done({}) 代表无改变
+   - //$done({}) 代表请求/响应无改变
+7.'$task' //网络请求
+   - Promise fetch({
+      url:'', //路径
+      method:'',//请求类型
+      headers:'',//请求头,
+      body:'',//请求体
+      opts:{
+         'filter':'脚本字符串,内含唯一变量body,需使用return返回的结果<16kb',//响应过滤,响应体>512kb时可使用,对网络扩展的内存占用更少
+         'policy':'direct',//带有特定策略执行请求,例如服务器地理位置检查
 
+      }
+
+     }) //请求
+8.'$environment' //环境对象,任务和重写脚本中可使用
+   - sourcePath //原始路径
+   - params //
+9.'$configuration'
+   - Promise sendMessage({
+      action:'',
+        -'dns_get_placeholder_ip/dns_update_cache/dns_clear_cache' //获取ip映射/更新dns缓存/清楚缓存
+        -'get_traffic_statistics' //流量统计
+        -'url_latency_benchmark' //url延迟测试
+          //content: ["Node-001","Node-002","Node-003","Node-004"]
+        -'get_server_description' //用于event-interaction脚本,获取服务器描述
+        -'get_policy_state'
+        -'get_customized_policy'
+          //content: PolicyName | ["PolicyName1","PolicyName2"]
+        -'set_policy_state' //只适用static or proxy策略
+          //content: {"proxy": "Node-002", "cPolicy": "Node-007"}
+        -'get_running_mode'
+        -'set_running_mode'
+          //content: { "running_mode": "all_proxy|all_direct|filter" }
+      content: Object | String | Array,
+   })
 
 ```
+
+
+
+# [http_backend]
 
--- scripts/ikuuuSign.js
+++ scripts/ikuuuSign.js
@@ -1,50 +1,91 @@
 const key = 'ikuuu';
-const url = 'https://ikuuu.de/user/checkin';
-
 const vals = $prefs.valueForKey(key);
 if (vals !== undefined) {
-    const arr = vals.split('&');
-    const ps = [];
-    for (item of arr) {
-        const p = new Promise((ok, rej) => {
-            let emailKey = item;
-            const ck = $prefs.valueForKey(emailKey);
-            const req = {
-                url: url,
-                method: 'POST',
-                headers: {
-                    Accept: 'application/json, text/javascript, */*; q=0.01',
-                    'Accept-Encoding': 'gzip, deflate, br, zstd',
-                    'Accept-Language': 'zh-CN,zh;q=0.9',
-                    Cookie: ck,
-                    Origin: 'https://ikuuu.de',
-                    Referer: 'https://ikuuu.de/user',
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36',
-                    'X-Requested-With': 'XMLHttpRequest'
-                }
-            };
-            $task.fetch(req).then(res => {
-                const body = JSON.parse(res.body);
-                ok(`${emailKey}签到成功：${body.msg}`);
-            }, err => {
-                rej(`${emailKey}签到失败：${err.error}`)
-            });
-        });
-        ps.push(p);
+    const arr = JSON.parse(vals);
+    const ps1 = [];
+    for(let i of arr){
+        ps1.push(loginUp({
+            email:i.split('&')[0],
+            passwd:i.split('&')[1]
+        }));
     }
-    Promise.all(ps).then(res=>{
-        console.log(res.reduce((a,b)=>a+'\n'+b,''))
-        $done();
-    }).catch(rej=>{
-        console.log(rej);
-        $done();
+    Promise.all(ps1).then(res=>{
+        console.log('完成');
     });
+    // const arr = vals.split('&');
+    // const ps = [];
+    // for (item of arr) {
+    //     let emailKey = item;
+    //     const ck = $prefs.valueForKey(emailKey);
+    //     ps.push(signUp(emailKey,ck));
+    // }
+    // Promise.all(ps).then(res => {
+    //     console.log(res.reduce((a, b) => a + '\n' + b, ''))
+    //     $done();
+    // }).catch(rej => {
+    //     console.log(rej);
+    //     $done();
+    // });
 
-}else{
+} else {
+    console.log('ikuuu签到失败,没有提供用户凭证');
     $done();
 }
 
-
+function loginUp(obj) {
+    const req = {
+        url: 'https://ikuuu.de/auth/login',
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json, text/javascript, */*; q=0.01',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'Referer': 'https://ikuuu.de/auth/login',
+            'Sec-Ch-Ua': '"Microsoft Edge";v="141", "Not?A_Brand";v="8", "Chromium";v="141"',
+            'Sec-Ch-Ua-Mobile': '?0',
+            'Sec-Ch-Ua-Platform': '"Windows"',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36 Edg/141.0.0.0',
+            'X-Requested-With': 'XMLHttpRequest'
+        },
+        body: {
+            host: 'ikuuu.de',
+            email: obj.email,
+            passwd: obj.passwd,
+            code: undefined
+        }
+    };
+    return new Promise((res, rej) => {
+        $task.fetch(req).then(response => {
+            console.log(response);
+            res(response);
+        }, reason => {
+            rej(reason);
+        });
+    })
+}
+function signUp(emailKey, ck) {
+    const req = {
+        url: 'https://ikuuu.de/user/checkin',
+        method: 'POST',
+        headers: {
+            Accept: 'application/json, text/javascript, */*; q=0.01',
+            'Accept-Encoding': 'gzip, deflate, br, zstd',
+            'Accept-Language': 'zh-CN,zh;q=0.9',
+            Cookie: ck,
+            Origin: 'https://ikuuu.de',
+            Referer: 'https://ikuuu.de/user',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36',
+            'X-Requested-With': 'XMLHttpRequest'
+        }
+    };
+    return new Promise((res, rej) => {
+        $task.fetch(req).then(res => {
+            const body = JSON.parse(res.body);
+            ok(`${emailKey}签到成功：${body.msg}`);
+        }, err => {
+            rej(`${emailKey}签到失败：${err.error}`)
+        });
+    });
+}
 
 
 
