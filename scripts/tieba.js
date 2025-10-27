b735a1e693042b9d069fb31ae0681fc0365ed9f2
kl <2967861744@qq.com>
2025-10-28 05:18:25 +0800

    t4

--- scripts/tieba.js
+++ scripts/tieba.js
@@ -1,11 +1,12 @@
 const url = $request.url;
 if(url.includes('/user/profile')){
     let obj = JSON.parse($response.body);
+    let types = ['game','banner','commerce'];
     delete obj['finance_tab'];
     delete obj['namoaixud_entry'];
     delete obj['duxiaoman_entry'];
-    obj['zone_info']=[];
-
+    obj['zone_info']=obj['zone_info'].filter(item=>!types.includes(item.type));
+    
     $done({body:JSON.stringify(obj)});
 }else{
     $done({})
