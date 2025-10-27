5928bd68d210f4061b60039fc104a02a6c3aeb60
kl <2967861744@qq.com>
2025-10-27 21:51:11 +0800

    z6

--- scripts/zhihu.js
+++ scripts/zhihu.js
@@ -1,7 +1,6 @@
 let html = $response.body;
 let str = `<script type='text/javascript'>
     let zhChecker1_2 = undefined;
-    let cleanStatu_k_1 = 0;
     function clearPopCheckKl_(){
         let signDoms = document.querySelectorAll('.signFlowModal');
         let closDoms = document.querySelectorAll('.Modal-closeButton');
@@ -12,9 +11,8 @@ let str = `<script type='text/javascript'>
         if(tipDoms.length>0){
             tipDoms[0].remove();
         }
-        if(!cleanStatu_k_1){
-            clearBannerCheckKl_2();
-        }
+        clearBannerCheckKl_2();
+        zhChecker1_2 = window.setInterval(clearPopCheckKl_,200);
     }
     function clearBannerCheckKl_2(){
         let bns = document.querySelector('.AppBanner');
@@ -22,9 +20,9 @@ let str = `<script type='text/javascript'>
         document.querySelector('.Question-main').removeAttribute('class');
         document.querySelectorAll('.css-dvccr2').forEach(i=>i.remove());
         document.querySelectorAll('.ContentItem-expandButton').forEach(i=>i.click());
-        cleanStatu_k_1=1;
+        cleanStatu_k_1=cleanStatu_k_1 + 1;
     }
-    zhChecker1_2=setInterval(clearPopCheckKl_,200);
+    clearPopCheckKl_();
 </script></html>`;
 html = html.replace(/<\/html>/,str);
 $done({body:html});
