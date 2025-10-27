01998af6e90dd0c400cee4f4d8c8a26629353674
kl <2967861744@qq.com>
2025-10-28 05:29:54 +0800

    t6

--- scripts/tieba.js
+++ scripts/tieba.js
@@ -5,14 +5,16 @@ if(url.includes('/user/profile')){
     delete obj['finance_tab'];
     delete obj['namoaixud_entry'];
     delete obj['duxiaoman_entry'];
-    obj['zone_info']=obj['zone_info'].filter(item=>!types.includes(item.type));
-    obj['zone_info']=obj['zone_info'].map(item=>{
-        if(item!=='common_func'){
+    delete obj['recom_naws_list'];
+    obj['zone_info']=obj['zone_info'].
+    filter(item=>!types.includes(item.type)).
+    map(item=>{
+        if(item.type!=='common_func'){
             return item;
         }
         item['common_func']['common_func_other']=[];
         return item;
-    });
+    })
     
     $done({body:JSON.stringify(obj)});
 }else{
