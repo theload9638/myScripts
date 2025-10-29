cc2fd3d2aba87801056ef694f0bb7917c87e256a
kl <2967861744@qq.com>
2025-10-30 06:04:49 +0800

    c2

--- scripts/csdn.js
+++ scripts/csdn.js
@@ -1,12 +1,13 @@
 const url = $request.url;
 
-if(url.includes('profile/app/get-ios-config')){
+if(url.includes('/get-ios-config?keys=homeTabTesting_7_B')){
     let body = JSON.parse($response.body);
     let p1 = ['大学生专区','学习','直播','问答','关注'];
     let p2 = ['首页','消息','我的'];
     body.data['homeTabTesting_7_B']['homeParentTagsConfig']['tags'] = body.data['homeTabTesting_7_B']['homeParentTagsConfig']['tags'].filter(item=>{
         return !p1.includes(item.parameter.titleName);
     });
+    body.data['homeTabTesting_7_B']['homeParentTagsConfig']['selectTag']=0;
     body.data['homeTabTesting_7_B']['tabbarConfig']['tabNamesList'] = body.data['homeTabTesting_7_B']['tabbarConfig']['tabNamesList'].filter(item=>{
         return p2.includes(item.title);
     });
