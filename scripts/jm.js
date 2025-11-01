b3949cdf6a88f54344d7fcd08106fbe5ce5f7458
kl <2967861744@qq.com>
2025-11-02 07:06:22 +0800

    j9

--- scripts/jm.js
+++ scripts/jm.js
@@ -19,7 +19,7 @@ if(url.includes('/ad_template')){
     html = html.replace(/resizeAd\(\)\s*;/g,'');
     html = html.replace(/<div\s*(?:class=\"ad-body\"([^>]*?))style=\".*?\"\s*>/g,'<div class="ad-body" $1 style="height:0px;">');
     html = html.replace(/<a.*?(?:target=\"_blank\").*?>[\s\S]*?<\/a>/g,'');
-    html = html.replace(/(<script[^<]*?src=\".*?ad-provider\.js\"\s*>[\s\S]*?)(<div class=\"group-notice\")>/g,'$2');
+    html = html.replace(/<script[^<]*?src=\".*?ad-provider\.js\"\s*>[\s\S]*?(<div class=\"group-notice\")>/g,'$1');
     html = html.replace(/<div\s*class=\"group-notice\">/g,'<div>');
     html = html.replace('/AD/g','');
     $done({body:JSON.stringify(html)});
