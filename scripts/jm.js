4bf2d91024f08ba9c9761d29b73f769f7f8e0186
kl <2967861744@qq.com>
2025-11-02 08:26:19 +0800

    j17

--- scripts/jm.js
+++ scripts/jm.js
@@ -13,8 +13,7 @@ if(url.includes('/ad_template')){
 }else if(url.includes('/v1/api.php')){
     let body = JSON.parse($response.body);
     body['zones']=[];
-    body['renderers']['native_ad']['content']='';
-    body['renderers']['banner']['content']='';
+    body['renderers']={};
     $done({body: JSON.stringify(body)});
 }else if(url.includes('/chapter_view_template')){
     let html = $response.body;
