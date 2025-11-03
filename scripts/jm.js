dfc1e3c0d8425cb3a95d3cc08941d67363a16273
kl <2967861744@qq.com>
2025-11-04 01:39:40 +0800

    j2

--- scripts/jm.js
+++ scripts/jm.js
@@ -2,9 +2,7 @@ const url = $request.url;
 
 if(url.includes('/ad_template')){
     let html = $response.body;
-    Array.from(html.matchAll(/<style>.*?<\/style>/gs)).forEach(match=>{
-        html = html.replace(match,'');
-    });
+    html = html.replace(/<style>.*?<\/style>/gs,'');
     html = html.replace(/<script>.*?(?=ad_encode).*?<\/script>/gs,'');
     html = html.replace(/<a.*?>.*?<\/a>/gs,'');
     html = html.replace(/resizeAd\(\)\s*;/g,'');
