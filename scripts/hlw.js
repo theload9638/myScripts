b9a4b85d01c086885eb34b6c0c1c3d4d8d58432a
kl <2967861744@qq.com>
2025-11-05 02:09:48 +0800

    fds3

--- scripts/hlw.js
+++ scripts/hlw.js
@@ -1,8 +1,7 @@
 let html = $response.body;
 const url = $request.url;
 if (html) {
-    html = html.replace(/<style>/g,'<style> .addbox,.modal,.slider-banners,.slider-wrapper,.infomation,.footer{display:none !important;} ');
-    html = html.replace(/(<div\s*class=\"video-item\s*\"([^>]*)>)(.*?(?!<\/h3>).*?<\/div>)/gs,'<div style="display:none !important;" class="video-item "$2>$3');
+    html = html.replace(/<style>/g,'<style> .addbox,.modal,.slider-banners,.slider-wrapper,.infomation,.footer{display:none !important;} .video-item{margin-top: .65rem;width:100%;display:none;} .video-item:has(h3){display:block !important;}');
     if(url.includes('/archives')){
         html = html.replace(/<style>/g,'<style> .list-sec-top,.list-sec,.common{display:none !important;}');
     }
