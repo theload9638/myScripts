d93cc642d3257f838534f51fa891fca7c6e8a748
kl <2967861744@qq.com>
2025-11-05 01:03:05 +0800

    bxccvw3

--- scripts/hlw.js
+++ scripts/hlw.js
@@ -1,5 +1,9 @@
 const html = $response.body;
 const url = $request.url;
+if(!$response.headers['Content-Type'].includes('text/html')){
+    $done({});
+    return
+}
 if (/(heiliao|18hlw)(?!\/index)/.test(url) && html) {
     html = html.replace(/<style>/g,'<style> .addbox,.modal,.slider-banners,.slider-wrapper,.infomation,.footer{display:none !important;} ');
     html = html.replace(/<div\s*class=\"video-item\"[^>]*>.*?(?!<\/h3>).*?<\/div>/gs,'');
