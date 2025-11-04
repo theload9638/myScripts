fc7cdfbeb8a3c1d77e9f81590bdcb51572a5f872
kl <2967861744@qq.com>
2025-11-05 00:22:53 +0800

    h1

---
+++ scripts/hlw.js
@@ -0,0 +1,11 @@
+const html = $response.body;
+const url = $request.url;
+if (html) {
+    html = html.replace('<style>','<style> .modal{display:none !important;} .slider-banners{display:none !important;} .slider-wrapper{display:none !important;} .infomation{display:none !important;} .footer{display:none !important;}');
+    html = html.replace(/<div\s*class=\"video-item\"[^>]*>.*?(?!<\/h3>).*?<\/div>/gs,'');
+    if(url.includes('/archives')){
+        html = html.replace('<style>','<style>.list-sec-top{display:none !important;} .list-sec{display:none !important;} .common{display:none !important;}');
+    }
+}
+$done({ body: html });
+
