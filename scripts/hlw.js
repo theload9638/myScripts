32216eb5fa840937f10fbc0d27517ba967daa0a5
kl <2967861744@qq.com>
2025-11-05 00:55:11 +0800

    are

--- scripts/hlw.js
+++ scripts/hlw.js
@@ -1,10 +1,10 @@
 const html = $response.body;
 const url = $request.url;
-if (/heiliao|18hlw/.test(url) && html) {
-    html = html.replace(/<style>/g,'<style> .addbox{display:none !important;} .modal{display:none !important;} .slider-banners{display:none !important;} .slider-wrapper{display:none !important;} .infomation{display:none !important;} .footer{display:none !important;} ');
+if (/(heiliao|18hlw)(?!\/index)/.test(url) && html) {
+    html = html.replace(/<style>/g,'<style> .addbox,.modal,.slider-banners,.slider-wrapper,.infomation,.footer{display:none !important;} ');
     html = html.replace(/<div\s*class=\"video-item\"[^>]*>.*?(?!<\/h3>).*?<\/div>/gs,'');
     if(url.includes('/archives')){
-        html = html.replace(/<style>/g,'<style>.list-sec-top{display:none !important;} .list-sec{display:none !important;} .common{display:none !important;}');
+        html = html.replace(/<style>/g,'<style> .list-sec-top,.list-sec,.common{display:none !important;}');
     }
     $done({ body: html });
 }else{
