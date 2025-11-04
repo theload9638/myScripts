d8838a306d988baec6971453e2cc1720a15bc5c6
kl <2967861744@qq.com>
2025-11-05 01:33:32 +0800

    432

--- scripts/hlw.js
+++ scripts/hlw.js
@@ -2,7 +2,7 @@ let html = $response.body;
 const url = $request.url;
 if (html) {
     html = html.replace(/<style>/g,'<style> .addbox,.modal,.slider-banners,.slider-wrapper,.infomation,.footer{display:none !important;} ');
-    html = html.replace(/<div\s*class=\"video-item\"[^>]*>.*?(?!<\/h3>).*?<\/div>/gs,'');
+    html = html.replace(/(<div\s*class=\"video-item\s*\"([^>]*)>)(.*?(?!<\/h3>).*?<\/div>)/gs,'<div style="display:none !important;" class="video-item "$2>$3');
     if(url.includes('/archives')){
         html = html.replace(/<style>/g,'<style> .list-sec-top,.list-sec,.common{display:none !important;}');
     }
