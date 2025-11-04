fae937c8f95aa88f41e2c1df5f2dcbf01c166c93
kl <2967861744@qq.com>
2025-11-05 04:54:26 +0800

    q1

---
+++ scripts/qd.js
@@ -0,0 +1,27 @@
+const url = $request.url;
+
+if(url.includes('/v3/user/getaccountpage')){
+    let body = JSON.parse($response.body);
+    let v1 = ['游戏中心','卡牌广场','周边商城'];
+    let v2 = ['我发布的','关注收藏','我的全订','浏览记录'];
+    body['Data']['BenefitButtonList']=body['Data']['BenefitButtonList'].filter(i=>!v1.includes(i['Name']));
+    body['Data']['FunctionButtonList']=body['Data']['FunctionButtonList'].filter(i=>v2.includes(i['Name']));
+    body['Data']['BottomButtonList']=body['Data']['BottomButtonList'].filter(i=>i['Name'].includes('帮助'));
+    $done({body:JSON.stringify(body)});
+}else if(url.includes('/v1/client/getconf')){
+    let bd = JSON.parse($response.body);
+    delete bd['Data']['ActivityPopup'];
+    delete bd['Data']['ActivityIcon'];
+    delete bd['Data']['BookShelfBottomIcons'];
+    bd['Data']['AdVideoPositionConfig']=[];
+    bd['Data']['ClientLocalNotify2']=[];
+    $done({body:JSON.stringify(bd)});
+}else if(url.includes('/v1/user/getsimplediscover')){
+    let bd = JSON.parse($response.body);
+    let v1 =['游戏','游戏中心f','周边商城','卡牌广场','热门角色'];
+    bd['Data']['Items'] = bd['Data']['Items'].filter(i=>!v1.includes(i['ShowName']));
+    $done({body:JSON.stringify(bd)});
+}
+else{
+    $done({});
+}
