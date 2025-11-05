6d0ff198cf1bea5c7bbd532a8996d191d04ce5ef
kl <2967861744@qq.com>
2025-11-06 00:01:47 +0800

    va12

--- scripts/hlw.js
+++ scripts/hlw.js
@@ -6,13 +6,6 @@ if (/^https?:\/\/heiliao\.com(?!\/(index|static))/.test(url)) {
         html = html.replace(/<style>/g,'<style> .list-sec-top,.list-sec,.common{display:none !important;}');
     }
     $done({ body: html });
-}else if(/^https?:\/\/(www\.)?91cg1\.com(?!\/(index|static))/.test(url)){
-    let html = $response.body;
-    html = html.replace(/<style>/g,"<style> article,.adspop,horizontal-banner,#foot-menu,#footer{display:none !important;} article:has(span[itemprop='datePublished']){display:block !important;}")
-    if(url.includes('/archives')){
-        html = html.replace(/<style>/g,'<style> .article-ads-btn,.post-content,#comments{display:none !important;}');
-    }
-    $done({ body: html });
 }
 else{
     $done({});
