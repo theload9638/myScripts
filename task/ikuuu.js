4e0306b29fd08cef25c624c11f0171fd25d4e36e
kl <2967861744@qq.com>
2025-11-09 09:00:35 +0800

    x1

--- task/ikuuu.js
+++ task/ikuuu.js
@@ -22,7 +22,7 @@ if (vals !== undefined) {
                 console.log(msg);
             });
         } catch (e) {
-            console.log(`error：${typeof e==='string'?e:JSON.stringify(e)}`);
+            console.log((typeof e === 'string')?e:`error：${e.error}\nopts：${e.opts?JSON.stringify(e.opts):null}`);
         } finally {
             $done();
         }
