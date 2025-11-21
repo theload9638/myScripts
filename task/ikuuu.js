13896bf86381719decc5871be5236a8ac805a4ec
kl <2967861744@qq.com>
2025-11-21 12:56:40 +0800

    t6

--- task/ikuuu.js
+++ task/ikuuu.js
@@ -12,7 +12,7 @@ if (vals !== undefined) {
         try {
             const res = await Promise.allSettled(ps1);
             failed = res.filter(i => i.status === 'rejected').map(i => i.reason);
-            let ps2 = res.filter(i => i.status === 'fulfilled').map(msg => {
+            let ps2 = res.filter(i => i.status === 'fulfilled').map(i=>i.reason).map(msg => {
                 if (msg.headers.hasOwnProperty('Set-Cookie')) {
                     let ck = msg.headers['Set-Cookie'].replace(/path=\/(,)?\s?/gi, '').
                         replace(/expires=[^;]+?;\s?/gi, '').trim();
