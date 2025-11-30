4b0e155b7f892b97e7d1a0c4c8b9ca470c14f41d
kl <2967861744@qq.com>
2025-12-01 04:50:49 +0800

    a1

--- scripts/hlw.js
+++ scripts/hlw.js
@@ -1,7 +1,12 @@
 const url = $request.url;
 if (/^https?:\/\/heiliao\.com(?!\/(index(?!\/search)|static))/.test(url)) {
     let html = $response.body;
-    html = html.replace(/<style>/g,'<style> .addbox,.modal,.slider-banners,.slider-wrapper,.infomation,.footer{display:none !important;} .video-item{margin-top: .65rem;width:100%;display:none;} .video-item:has(h3){display:block !important;}');
+    let str = '<style> .addbox,.modal,.slider-banners,.slider-wrapper,.infomation,.footer{display:none !important;} ';
+    let str1 =' .video-item{margin-top: .65rem;width:100%;display:none;} .video-item:has(h3){display:block !important;}';
+    if(url.includes('/index/search')){
+       str=str+str1;   
+    }
+    html = html.replace(/<style>/g,str);
     if(url.includes('/archives')){
         html = html.replace(/<style>/g,'<style> .list-sec-top,.list-sec,.common{display:none !important;}');
     }
