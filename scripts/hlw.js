eb3ccaca4da0ff9a507b6cc1abd6f72584cc876e
kl <2967861744@qq.com>
2025-12-01 04:40:59 +0800

    da1

--- scripts/hlw.js
+++ scripts/hlw.js
@@ -1,5 +1,5 @@
 const url = $request.url;
-if (/^https?:\/\/heiliao\.com(?!\/(index|static))/.test(url)) {
+if (/^https?:\/\/heiliao\.com(?!\/(index(?!\/search)|static))/.test(url)) {
     let html = $response.body;
     html = html.replace(/<style>/g,'<style> .addbox,.modal,.slider-banners,.slider-wrapper,.infomation,.footer{display:none !important;} .video-item{margin-top: .65rem;width:100%;display:none;} .video-item:has(h3){display:block !important;}');
     if(url.includes('/archives')){
