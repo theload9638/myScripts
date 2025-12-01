c570d2e03a505638d61d91fc635b9f6a099f74a9
kl <2967861744@qq.com>
2025-12-01 10:24:26 +0800

    k1

--- scripts/hlw.js
+++ scripts/hlw.js
@@ -3,7 +3,7 @@ if (/^https?:\/\/heiliao\.com(?!\/(index(?!\/search)|static))/.test(url)) {
     let html = $response.body;
     let str = '<style> .addbox,.modal,.slider-banners,.slider-wrapper,.infomation,.footer{display:none !important;} ';
     let str1 =' .video-item{margin-top: .65rem;width:100%;display:none;} .video-item:has(h3){display:block !important;}';
-    if(url.includes('/index/search')){
+    if(!url.includes('/index/search')){
        str=str+str1;   
     }
     html = html.replace(/<style>/g,str);
