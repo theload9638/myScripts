bde72b2ea2cccded7c0681a90f42f7672d29b32e
kl <2967861744@qq.com>
2026-01-22 05:01:59 +0800

    da11

--- scripts/trq.js
+++ scripts/trq.js
@@ -1,5 +1,9 @@
 const url = $request.url;
 if(/^https?:\/\/www\.tongrenquan\.org\/tongren\/(.*)+?.html/.test(url)){
+    if(url.includes('9471.html')){
+        $done({});
+        return
+    }
      let html = $response.body;
      let styleStr = '<style>.google-auto-placed,.adsbygoogle,.adsbygoogle-noablate{display:none !important;} * {background:#595757 !important;}</style>';
      html = html.replace(/<\/head>/,styleStr+'</head>');
