4711badc687388e081b437856d0d8ab43f700624
kl <2967861744@qq.com>
2026-01-22 04:44:54 +0800

    das1

--- scripts/trq.js
+++ scripts/trq.js
@@ -1,5 +1,7 @@
 const url = $request.url;
-
+let type = $response.headers['content-type'] || $response.headers['Content-Type'];
+console.log(type);
+console.log($response.headers['content-type']);
 if(/^https?:\/\/www\.tongrenquan\.org\/tongren\/(.*)+?.html/.test(url)){
      let html = $response.body;
      let styleStr = '<style>.google-auto-placed,.adsbygoogle,.adsbygoogle-noablate{display:none !important;} *{background:#595757 !important;}</style>';
