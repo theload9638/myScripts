59684c59aaa6458554a5d1953d39f2ae586c2d8d
kl <2967861744@qq.com>
2026-01-22 16:24:28 +0800

    sad2

--- scripts/trq.js
+++ scripts/trq.js
@@ -1,13 +1,5 @@
 const url = $request.url;
-if(/^https?:\/\/www\.tongrenquan\.org\/tongren\/(.*)+?.html/.test(url)){
-    if(url.includes('9471')){
-        $done();
-        return
-    }
-     let html = $response.body;
-     let styleStr = '<style>.google-auto-placed,.adsbygoogle,.adsbygoogle-noablate{display:none !important;} * {background:#595757 !important;}</style>';
-     html = html.replace(/<\/head>/,styleStr+'</head>');
-     $done({ body: html });
-}else{
-    $done();
-}
+let html = $response.body;
+let styleStr = '<style>.google-auto-placed,.adsbygoogle,.adsbygoogle-noablate{display:none !important;} * {background:#595757 !important;}</style>';
+html = html.replace(/<\/head>/, styleStr + '</head>');
+$done({ body: html });
