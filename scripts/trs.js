08bdc48179a6847c6c4dff377efc83dd95096920
kl <2967861744@qq.com>
2026-01-22 20:40:18 +0800

    t1

--- scripts/trs.js
+++ scripts/trs.js
@@ -2,6 +2,7 @@ const url = $request.url;
 let uint8Buffer = new Uint8Array($response.bodyBytes);
 let styleStr = '<style>*{background:#595757 !important;}</style>';
 let html = new TextDecoder('utf-8', { fatal: false, ignoreBOM: true }).decode(uint8Buffer);
+console.log(html);
 html = html.replace(/<\/head>/, styleStr + '</head>');
 let utf8Bytes = new TextEncoder().encode(html);
 $done({ bodyBytes: utf8Bytes.buffer });
