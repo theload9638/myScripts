f2e0995a247d07d0c6db5074818d8bbbfdd900e5
kl <2967861744@qq.com>
2026-01-23 04:13:09 +0800

    13

--- scripts/trq.js
+++ scripts/trq.js
@@ -1,7 +1,7 @@
 const uint8Buffer = new Uint8Array($response.bodyBytes);
 let html = new TextDecoder('gb2312',{ fatal: false, ignoreBOM: true }).decode(uint8Buffer);
 
-let styleStr = '<style>.GoogleActiveViewInnerContainer,.google-auto-placed,.adsbygoogle,.adsbygoogle-noablate{display:none !important;} * {background:#595757 !important;} .reading{ padding: 0px !important;}</style>';
+let styleStr = '<style>.GoogleActiveViewInnerContainer,.google-auto-placed,.adsbygoogle,.adsbygoogle-noablate{display:none !important;} * {background:#9c9999 !important;} .reading{ padding: 0px !important;}</style>';
 html = html.replace(/<\/head>/, styleStr + '</head>');
 html = html.replace('gb2312','utf-8');
 
