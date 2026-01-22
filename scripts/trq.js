3cf16e4330a4d24f38362acc3f45def6d9bad8d0
kl <2967861744@qq.com>
2026-01-22 16:40:02 +0800

    ds2

--- scripts/trq.js
+++ scripts/trq.js
@@ -1,5 +1,13 @@
 const url = $request.url;
-let html = $response.body;
-let styleStr = '<style>.google-auto-placed,.adsbygoogle,.adsbygoogle-noablate{display:none !important;} * {background:#595757 !important;}</style>';
-html = html.replace(/<\/head>/, styleStr + '</head>');
-$done({ body: html });
+if(/^https?:\/\/www\.tongrenquan\.org\/tongren\/(.*)+?.html/.test(url)){
+    if(url.includes('9471.html')){
+        $done({});
+        return
+    }
+     let html = $response.body;
+     let styleStr = '<style>.google-auto-placed,.adsbygoogle,.adsbygoogle-noablate{display:none !important;} * {background:#595757 !important;}</style>';
+     html = html.replace(/<\/head>/,styleStr+'</head>');
+     $done({ body: html });
+}else{
+    $done();
+}
