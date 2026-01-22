7b0e977b1e78b485880d118eb629256fdac45e3a
kl <2967861744@qq.com>
2026-01-22 20:55:08 +0800

    fa1

--- scripts/trs.js
+++ scripts/trs.js
@@ -1,8 +1,7 @@
 const uint8Buffer = new Uint8Array($response.bodyBytes);
-const html = iconv.decode(uint8Buffer, 'gb2312');
-
+const html = new TextDecoder('gb2312',{ fatal: false, ignoreBOM: true }).decode(uint8Buffer);
 const styleStr = '<style>*{background:#595757 !important;}</style>';
-const processedHtml = html.replace(/<\/head>/, styleStr + '</head>');
-
-const gb2312Bytes = iconv.encode(processedHtml, 'gb2312');
+let processedHtml = html.replace(/<\/head>/, styleStr + '</head>');
+processedHtml = processedHtml.replace('gb2312','utf-8');
+const gb2312Bytes = new TextEncoder().encode(processedHtml);
 $done({ bodyBytes: gb2312Bytes.buffer });
