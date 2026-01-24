fe18aa275c2d917132cbe09cd6cd005f5635af34
kl <2967861744@qq.com>
2026-01-25 02:10:36 +0800

    gs2

--- scripts/trq.js
+++ scripts/trq.js
@@ -1,7 +1,7 @@
 const uint8Buffer = new Uint8Array($response.bodyBytes);
 let html = new TextDecoder('gb2312',{ fatal: false, ignoreBOM: true }).decode(uint8Buffer);
 
-let styleStr = '<style>#ad-container,.GoogleActiveViewInnerContainer,.google-auto-placed,video,#Image,#smx_wrap,#video-ad-ui,#aswift_9,#aswift_9_host,.adsbygoogle,.adsbygoogle-noablate{display:none !important;} * {background:#9c9999 !important;} .reading{ padding: 0px !important;}</style>';
+let styleStr = '<style>#ad-container,.GoogleActiveViewInnerContainer,.google-auto-placed,video,#Image,#smx_wrap,#video-ad-ui,#aswift_9,#aswift_9_host,.adsbygoogle,.adsbygoogle-noablate{display:none !important;} * {background: #9c9999 !important;} .reading{ padding: 0px !important;}</style>';
 html = html.replace(/<\/head>/, styleStr + '</head>');
 html = html.replace('gb2312','utf-8');
 
--- scripts/trs.js
+++ scripts/trs.js
@@ -1,6 +1,6 @@
 const uint8Buffer = new Uint8Array($response.bodyBytes);
 const html = new TextDecoder('gb2312',{ fatal: false, ignoreBOM: true }).decode(uint8Buffer);
-const styleStr = '<style>*{background:#9c9999 !important;}</style>';
+const styleStr = '<style>*{background: #9c9999 !important;}</style>';
 let processedHtml = html.replace(/<\/head>/, styleStr + '</head>');
 processedHtml = processedHtml.replace('gb2312','utf-8');
 const gb2312Bytes = new TextEncoder().encode(processedHtml);
