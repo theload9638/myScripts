1da474541b0530438fc4fa720fe4e39eaa3a31b1
kl <2967861744@qq.com>
2026-01-25 14:55:15 +0800

    kmk

--- scripts/bz.js
+++ scripts/bz.js
@@ -1,6 +1,6 @@
 let html = $response.body;
 let styleStr = '<style>.slide-ad,.footer,.slide,#ad,#immersive-translate-popup{display:none !important;} * {background: #dbd7d7 !important;}</style>';
 html = html.replace(/<\/head>/, styleStr + '</head>');
-html = html.replace(/class=\"slide-ad\"/g,'class="a"');
-html = html.replace(/<div\s*id=\"immersive-translate-popup\"(.*?)/s,'</html>');
+html = html.replace(/class=\"slide-ad\"/g,'');
+html = html.replace(/<div\s*class=\"footer\"(.*)?/s,'</div></body></html>');
 $done({ body: html });
