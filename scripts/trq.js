799f832c6829ecda74b9f51d2347494d58dcb15c
kl <2967861744@qq.com>
2026-01-25 15:56:17 +0800

    ga

--- scripts/trq.js
+++ scripts/trq.js
@@ -2,9 +2,7 @@ const url = $request.url;
 const uint8Buffer = new Uint8Array($response.bodyBytes);
 let html = new TextDecoder('gb2312',{ fatal: false, ignoreBOM: true }).decode(uint8Buffer);
 
-let styleStr = '<style>#ad-container,.GoogleActiveViewInnerContainer,.google-auto-placed,video,#Image,#smx_wrap,#video-ad-ui,#aswift_9,#aswift_9_host,.adsbygoogle,.adsbygoogle-noablate{display:none !important;} * {background: #9c9999 !important;} .reading{ padding: 0px !important;}</style>';
-html = html.replace(/<\/head>/, styleStr + '</head>');
-html = html.replace('gb2312','utf-8');
+let styleStr = '#ad-container,.GoogleActiveViewInnerContainer,.google-auto-placed,video,#Image,#smx_wrap,#video-ad-ui,#aswift_9,#aswift_9_host,.adsbygoogle,.adsbygoogle-noablate{display:none !important;} * {background: #9c9999 !important;} .reading{ padding: 0px !important;}';
 html = html.replace(/<div\s*id=\"immersive-translate-popup\"(.*?)/s,'</html>');
 
 if(/[a-zA-Z_]+\/\d+\.html$/.test(url)){
@@ -13,7 +11,11 @@ if(/[a-zA-Z_]+\/\d+\.html$/.test(url)){
     html = html.replace(/<div\s*class=\"head\">(.*)?<div\s*class=\"topReadContent\"([^>]*?)>/s,'<div class="topReadContent">');
     html = html.replace(/<ins(.*?)<\/ins>/gs,'');
     html = html.replace(/<iframe(.*?)<\/iframe>/gs,'');
+    styleStr= styleStr +' .next_pre,.hotlist{display:none !important;}';
 }
+
+html = html.replace(/<\/head>/,'<style>'+styleStr+'</style></head>');
+html = html.replace('gb2312','utf-8');
 
 const utf8Bytes = new TextEncoder().encode(html);
 $done({ bodyBytes: utf8Bytes.buffer });
