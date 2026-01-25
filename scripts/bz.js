23177d96d14637a8211ee496f69c8b5b6186e388
kl <2967861744@qq.com>
2026-01-25 17:00:01 +0800

    bd21

--- scripts/bz.js
+++ scripts/bz.js
@@ -1,5 +1,5 @@
 let html = $response.body;
-let styleStr = '<style>.slide-ad,.footer,.slide,#ad,#immersive-translate-popup{display:none !important;} * {background: #dbd7d7 !important;}</style>';
+let styleStr = '<style>.slide-ad,.footer,.slide,#ad,#immersive-translate-popup{display:none !important;} * {background: #706a6a !important;}</style>';
 html = html.replace(/<\/head>/, styleStr + '</head>');
 html = html.replace(/class=\"slide-ad\"/g,'');
 html = html.replace(/<div\s*class=\"footer\"(.*)?/s,'</div></body></html>');
--- scripts/cool18.js
+++ scripts/cool18.js
@@ -1,4 +1,4 @@
 let html = $response.body;
-let styleStr = '<style>td:has(iframe),.bottom-nav,.comment-section,.post-list,.view_ad_bottom,.vote-section,.view-gift,.view_tools_box,.adv-6park,.root--26nWL,.bottomRight--h0VsQ,.slideAnimation--2ih2G{display:none !important;} * {background:#a8a1a1 !important;} a:link{color: #fbf6f7; !important;}</style>';
+let styleStr = '<style>td:has(iframe),.bottom-nav,.comment-section,.post-list,.view_ad_bottom,.vote-section,.view-gift,.view_tools_box,.adv-6park,.root--26nWL,.bottomRight--h0VsQ,.slideAnimation--2ih2G{display:none !important;} * {background: #706a6a !important;} a:link{color: #fcfafb; !important;}</style>';
 html = html.replace(/<\/head>/, styleStr + '</head>');
 $done({ body: html });
