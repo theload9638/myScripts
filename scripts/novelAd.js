fb2298766dcee70c22db1fddf0bd66850f96d4ee
kl <2967861744@qq.com>
2026-01-29 23:23:51 +0800

    ad11

--- scripts/novelAd.js
+++ scripts/novelAd.js
@@ -17,6 +17,7 @@ if (type.includes("text")) {
         'diagramjawlineunhappy.com',
         'img.doppiocdn.com',
         'ttavej.com',
+        'www.interactivebrokers.com',
         'www.lovedate.cc',
         '29782a430255d79f2wap.jigool.org',
         's.magsrv.com',
@@ -30,11 +31,8 @@ if (type.includes("text")) {
         'wn.pos.baidu.com',
         'cpro.baidustatic.com',
         'banner',
-        'adv-',
-        'model-',
+        'ad-provider',
         '-model',
-        'ad-',
-        'ad_',
         '_ad',
         '_adv',
         '-ad',
@@ -44,16 +42,14 @@ if (type.includes("text")) {
     ];
     const escapeRegExp = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
     let html = $response.body;
-    let styleStr = 'ins,iframe,video,.banner,.ad,#ad{display:none !important;pointer-events: none !important;} div{ background-image:none !important;}';
+    let styleStr = 'ins,iframe,video,.banner,.ad,#ad,.footer,#footer{display:none !important;pointer-events: none !important;} div{ background-image:none !important;}';
     let bgColor = '#494747';
     
     html = html.replace(/<ins(.*?)<\/ins>/gs, '');
     html = html.replace(/<iframe(.*?)<\/iframe>/gs, '');
     html = html.replace(/<video(.*?)<\/video>/gs, '');
 
-    if (/^https?:\/\/syosetu\.org/.test(url)) {
-        styleStr += '#footer{display:none !important;pointer-events: none !important;}';
-    } else if (/^https?:\/\/hlib\.cc/.test(url)) {
+    if (/^https?:\/\/hlib\.cc/.test(url)) {
         styleStr += '#suggest,#rnlist,#comments,#exo-native-widget-5098390-adX3C,.my-2,.pHS5vbgQ_main_outstream,.exo_wrapper_show,.container-xl{display:none !important;pointer-events: none !important;}';
         html = html.replace(/alert\(/g, "//");
     } else if (/^https?:\/\/www\.uaa(.*?)\.com/.test(url)) {
@@ -63,19 +59,18 @@ if (type.includes("text")) {
             styleStr +=' .menu_box,.more_menu,.avatar{display:none !important;pointer-events: none !important;}';
         }
     } else if (/^https?:\/\/www\.cool18\.com/.test(url)) {
-        styleStr += '.bottom-nav,.comment-section,.post-list,.view_ad_incontent,.view_ad_bottom,.vote-section,.view-gift,.view_tools_box,.adv-6park,.root--26nWL,.bottomRight--h0VsQ,.slideAnimation--2ih2G{display:none !important;pointer-events: none !important;} * {background: ' + bgColor + ' !important;} a:link{color: #fcfafb; !important;}';
-        if (url.includes('app=forum&act=threadview')) {
+        styleStr += '.bottom-nav,.comment-section,.post-list,.vote-section,.view-gift,.view_tools_box,.root--26nWL,.bottomRight--h0VsQ,.slideAnimation--2ih2G{display:none !important;pointer-events: none !important;} * {background: ' + bgColor + ' !important;} a:link{color: #fcfafb; !important;}';
+        if (url.includes('act=threadview')) {
             html = html.replace(/<div\s*class=\"ad-container\">(.*?)<div\s*class=\"main-content\">/s, '<div class="main-content">');
             styleStr = styleStr + ".subtitle-container,.bottom-nav,.comment-section,.post-list,.ai-detection-feedback{display:none !important;pointer-events: none !important;}";
         }
     } else if (/^https?:\/\/m\.diyibanzhu\.(me|rest)/.test(url)) {
-        styleStr += '.slide-ad,.footer,.slide{display:none !important;pointer-events: none !important;} * {background: ' + bgColor + ' !important;}';
-        html = html.replace(/class=\"slide-ad\"/g, '');
+        styleStr += '.slide{display:none !important;pointer-events: none !important;} * {background: ' + bgColor + ' !important;}';
         if (url.includes('action=article')) {
             styleStr = styleStr + ' .header,.tuijian,#announceinfo{display:none !important;pointer-events: none !important;}';
         }
     }else if(/^https?:\/\/m\.shuhaige\.net\/\d+\/\d+\.html/.test(url)){
-        styleStr += '.footer,.path,.tui,.bYtYBpFi,.tmwac{display:none !important;pointer-events: none !important;} * {background: ' + bgColor + ' !important;}';
+        styleStr += '.path,.tui,.bYtYBpFi,.tmwac{display:none !important;pointer-events: none !important;} * {background: ' + bgColor + ' !important;}';
         html = html.replace(/<script[^>]*>.*?<\/script>/gs,'');
         html = html.replace(/<div\s*style=\"margin:0;padding:0;outline:0;margin-top:15px\">/,'<div style="display:none !important;">')
         html = html.replace(/<([a-z]+)\s+style=\"display:\s*block;\s*z-index:\s*\d+;\s*position:\s*fixed;.*?\"><\/\1>/gi,'');
@@ -83,7 +78,7 @@ if (type.includes("text")) {
     domains.map(escapeRegExp).forEach(domain => {
         html = html.replace(new RegExp(`<([a-zA-Z0-9]+)\\s+[^>]*?(src|href|class|id)\\s*=\\s*(['"]).*?${domain}.*?\\3[^>]*?>`, 'gi'), '<$1 style="display:none !important;pointer-events: none !important;">');
     });
-    html = html.replace(new RegExp(`<([a-zA-Z0-9]+)\\s+[^>]*?(src|href)\\s*=\\s*(['"]).*?\\d+[a-zA-Z]+\.[a-zA-Z]+.\.(cc|com|xyz|net|org)(:?)*?\\3[^>]*?>`, 'gi'), '<$1 style="display:none !important;pointer-events: none !important;">');
+    html = html.replace(new RegExp(`<([a-zA-Z0-9]+)\\s+[^>]*?(src|href)\\s*=\\s*(['"]).*?\/\/\\d+[a-zA-Z]+\.[a-zA-Z]+.\.(cc|com|xyz|net|org)(:?)*?\\3[^>]*?>`, 'gi'), '<$1 style="display:none !important;pointer-events: none !important;">');
     if (styleStr !== '') {
         html = html.replace(/<\/head>/, '<style>' + styleStr + '</style></head>');
     }
