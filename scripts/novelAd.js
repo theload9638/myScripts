161e987809979992b05d9d738dcd744fb030b44f
kl <2967861744@qq.com>
2026-01-31 04:28:19 +0800

    af12

--- scripts/novelAd.js
+++ scripts/novelAd.js
@@ -107,6 +107,7 @@ if (type.includes("text")) {
                 }
             } else if (/https?:\/\/www\.tongrenxsw\.com/.test(url)) {
                 ignoreDivImg = false;
+                domains.splice(domains.indexOf('popup'),1);
                 styleStr += '.headerW,.navM,.searchBoxM,.about,.btnAddBook,.navM2,.recoBox2,.btnErrorW{display:none !important;pointer-events: none !important;}';
                 if (/\/book\/\w+(-\w+)?\.html/i.test(url)) {
                     html = html.replace(/<script\s*>[^>]*?<\/script>/gs, '');
