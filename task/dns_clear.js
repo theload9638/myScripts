e07549ac1a45fdbc61322ff70ef7babb02446898
kl <2967861744@qq.com>
2026-03-19 14:58:52 +0800

    add22

--- task/dns_clear.js
+++ task/dns_clear.js
@@ -1,9 +1,9 @@
 $configuration.sendMessage({action: "dns_clear_cache"}).then(resolve => {
     if (resolve.error) {
-        console.log(`dns缓存清除异常：${resolve.error}`);
+        console.log(`dns cache clear error ：${resolve.error}`);
     }
     if (resolve.ret) {
-        console.log('dns缓存清除结束!');
+        console.log('dns cache clear ok!');
     }
     $done();
 }, reject => {
