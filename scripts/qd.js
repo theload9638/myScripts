1519f39e08c60a3583e376e6215fec2293152ba0
kl <2967861744@qq.com>
2026-03-26 12:57:22 +0800

    fas1

--- scripts/qd.js
+++ scripts/qd.js
@@ -19,8 +19,6 @@
   #弹窗/卡片
 ^https?:\/\/(magev6|h5)\.if\.qidian\.com\/argus\/api\/v1\/popup\/batchget url jsonjq-response-body '.Data.PopupList=[]'
 ^https?:\/\/(magev6|h5)\.if\.qidian\.com\/argus\/api\/v1\/(bookshelf|freshman|popup|booksquare|freshman|bookshelf)\/(getTopOperation|freshmanGuidePopup|getfloatcard) url jsonjq-response-body '{"Message":"未命中策略","Result":-452000}'
-  #我的净化
-^https:\/\/(magev6|h5)\.if\.qidian\.com\/argus\/api\/v3\/user\/getaccountpage url script-response-body https://raw.githubusercontent.com/theload9638/myScripts/main/scripts/qd.js
   #弹窗广告配置
 ^https?:\/\/(magev6|h5)\.if\.qidian\.com\/argus\/api\/v1\/client\/getconf url script-response-body https://raw.githubusercontent.com/theload9638/myScripts/main/scripts/qd.js
 ^https?:\/\/(magev6|h5)\.if\.qidian\.com\/argus\/api\/v1\/client\/getconfSpecify url jsonjq-response-body '.Data.AdvideoPositionConfig=[]'
@@ -32,7 +30,6 @@
 ^https?:\/\/(magev6|h5)\.if\.qidian\.com\/argus\/api\/v1\/assembly\/toolbar url jsonjq-response-body '.|del(.Data.Toolbar.Adv)'
   #签到
 ^https?:\/\/(magev6|h5)\.if\.qidian\.com\/argus\/api\/v3/checkin\/getcurrentweekcheckininfo url script-response-body https://raw.githubusercontent.com/theload9638/myScripts/main/scripts/qd.js
-^https?:\/\/(magev6|h5)\.if\.qidian\.com\/argus\/api\/v2/checkin\/simpleinfo url script-response-body https://raw.githubusercontent.com/theload9638/myScripts/main/scripts/qd.js
 
 [mitm]
 hostname = magev6.if.qidian.com,h5.if.qidian.com
@@ -40,22 +37,7 @@ hostname = magev6.if.qidian.com,h5.if.qidian.com
 */
 
 const url = $request.url;
-if (url.includes('/getaccountpage')) {
-  let body = JSON.parse($response.body);
-  let v1 = ['游戏中心', '卡牌广场', '周边商城'];
-  let v2 = ['我发布的', '关注收藏', '我的全订', '浏览记录'];
-  body['Data']['BenefitButtonList'] = body['Data']['BenefitButtonList'].filter(i => !v1.includes(i['Name']));
-  body['Data']['FunctionButtonList'] = body['Data']['FunctionButtonList'].filter(i => v2.includes(i['Name']));
-  body['Data']['BottomButtonList'] = body['Data']['BottomButtonList'].filter(i => i['Name'].includes('帮助'));
-  body['Data']['Member']['SubTitle'] = '';
-  body['Data']['AccountBalance']['YdFreeBalance']=999;
-  body['Data']['AccountBalance']['YdBalance']=999;
-  body['Data']['AccountBalance']['QdFreeBalance']=999;
-  body['Data']['AccountBalance']['QdBalance']=999;
-  body['Data']['AccountBalance']['Coupon']=999;
-  body['Data']['AccountBalance']['NewCoupon']=999;
-  $done({ body: JSON.stringify(body) });
-} else if (url.includes('/v1/client/getconf')) {
+if (url.includes('/v1/client/getconf')) {
   let bd = JSON.parse($response.body);
   delete bd['Data']['ActivityPopup'];
   delete bd['Data']['AudioGuide'];
@@ -79,11 +61,5 @@ if (url.includes('/getaccountpage')) {
   bd['Data']['ReCheckPrice']=0;
   bd['Data']['IsMember']=1;
   bd['Data']['FreeRecheckChanceUsed']=0;
-  bd['Data']['Balance']=9999;
-  $done({ body: JSON.stringify(bd) });
-}else if(url.includes('/checkin/simpleinfo')){
-  let bd = JSON.parse($response.body);
-  bd['Data']['PushSwitchPopData']['ShowPop']=0;
-  bd['Data']['MemberFlag']=1;
   $done({ body: JSON.stringify(bd) });
 }
